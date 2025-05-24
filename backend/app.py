3d7cf910dd8d010784e2de51784d07d7c6e53903
Aditi Pillai <write2aditipillai@gmail.com>
2025-05-24 12:42:14 +0530

    backend commit final ig

--- backend/app.py
+++ backend/app.py
@@ -2,14 +2,16 @@ from flask import Flask
 from overpass import overpass
 from flask_cors import CORS
 from other_scrapers import other_scrapers_bp
+from processing import processing_bp
 app = Flask(__name__)
-# CORS(app)
+CORS(app)
 app.secret_key = "your_secret_key_here"
 
 CORS(overpass)
 # Register blueprints
 app.register_blueprint(overpass, url_prefix="/overpass")
 app.register_blueprint(other_scrapers_bp, url_prefix="/scraper")
+app.register_blueprint(processing_bp, url_prefix="/processing")
 
 @app.route("/")
 def index():
