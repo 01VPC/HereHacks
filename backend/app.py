83b92788a4a2bdbbf3adf6489a4a44189dfa1eb1
Aditi Pillai <write2aditipillai@gmail.com>
2025-05-24 09:00:05 +0530

    app

--- backend/app.py
+++ backend/app.py
@@ -3,9 +3,10 @@ from overpass import overpass
 from flask_cors import CORS
 from other_scrapers import other_scrapers_bp
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
 app.secret_key = "your_secret_key_here"
 
+CORS(overpass)
 # Register blueprints
 app.register_blueprint(overpass, url_prefix="/overpass")
 app.register_blueprint(other_scrapers_bp, url_prefix="/scraper")
